f4ac340d1386746ecdae27a7939efb0636306b27
Zhou <zhoujas27@7hills.org>
2026-02-09 09:34:38 -0500

    fixed constructor syntax, wrote out teacher class foundations, wrote getters for studnet and teacher, began with outlining time class

--- Teacher.java
+++ Teacher.java
@@ -2,11 +2,41 @@ public class Teacher {
 
 
 	//variables
+	private int idNum;
+	private String name;
+	private Course course1;
+	private Course course2;
 	//constructors
-	public Teacher {
-		
+	public Teacher(int id, String n, Course c1, Course c2) {
+		idNum = id;
+		name = n;
+		course1 = c1;
+		course2 = c2;
 	}	
 	//methods
+	
+	//getters
+	public int getID(){
+		return idNum;
+	}
+	public String getName(){
+		return name;
+	}
+	public Course getC1(){
+		return course1;
+	}
+	public Course getC2(){
+		return course2;
+	}
+	//toString
+	public String toString(){
+		sToString = "";
+		sToString = "id number: " + idNum;
+		sToString + "\nname: " + name;
+		sToString + "\n1st course: " + course1;
+		sToString + "\n2nd course: " + course2;
+		return sToString;
+	}
 
 
 
--- Time.java
+++ Time.java
@@ -1,11 +1,21 @@
 public class Time {
 
 	//variables
+	int month;
+	int day;
+	int year;
+	int hour;
+	int minute;
 	//constructors
-	public Time {
-		
+	public Time(int mon, int d, int y, int h, int min) {
+		month = mon;
+		day = d;
+		year = y;
+		hour = h;
+		minute = min;
 	}	
 	//methods
+	public 
 
 
 
